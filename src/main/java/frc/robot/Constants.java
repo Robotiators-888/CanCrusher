d9dd30b02e4b81050accc9075483d8f4136165d1
OmarKarim747 <superoctopus747@gmail.com>
2025-10-14 15:18:52 -0400

    Deleted unescesary code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -32,16 +32,5 @@ public final class Constants {
     public static final double kDriveDeadband = 0.05;
 }
 
-  public static class GroundIntakeConstants {
-    public static final int kGroundIntakeCanID = 51;
-    public static final double kGroundIntakeSpeed = -0.45;
-    public static final double kGroundEjectSpeed = 0.45;
-  }
-  public static class GroundPivotConstants {
-    public static final int kGroundPivotCanID = 52;
-    public static final double kIntakePos = 135;
-    public static final double kIntakeThreshold = 10; 
-    public static final double kStowPos = 0;
-    public static final double kScorePos = 25; 
-  }
+
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,15 +14,11 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.Constants.GroundIntakeConstants;
-import frc.robot.Constants.GroundPivotConstants;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.AutoCommand;
 import frc.robot.commands.DriveCommand;
 import frc.robot.subsystems.CANDriveSubsystem;
-import frc.robot.subsystems.SUB_GroundIntake;
-import frc.robot.subsystems.SUB_GroundPivot;
-import frc.robot.subsystems.SUB_Pneumatics;
+
 
 /**
  * This class is where the bulk of the robot should be declared. Since
@@ -36,9 +32,7 @@ import frc.robot.subsystems.SUB_Pneumatics;
 public class RobotContainer {
   // The robot's subsystems
   private static CANDriveSubsystem driveSubsystem = new CANDriveSubsystem();
-  public static SUB_GroundIntake groundIntake = SUB_GroundIntake.getInstance();
-  public static SUB_GroundPivot groundPivot = SUB_GroundPivot.getInstance();
-  public static SUB_Pneumatics pneumatics = SUB_Pneumatics.getInstance();
+
 
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
@@ -89,16 +83,6 @@ public class RobotContainer {
         () -> -Driver1.getRightX(),
         driveSubsystem));
 
-    groundIntake.setDefaultCommand(
-          new RunCommand(() -> groundIntake.groundIntakeDetection(()->groundPivot.nearIntakeSetpoint()), groundIntake));
-  
-    groundPivot.setDefaultCommand(
-          new RunCommand(() -> groundPivot.drivePivotPID(), groundPivot));
-
-  Driver1.start().onTrue(new InstantCommand(()-> groundPivot.changeSetpoint(GroundPivotConstants.kIntakePos)));
-    Driver1.leftStick().onTrue(new InstantCommand(()-> groundPivot.changeSetpoint(GroundPivotConstants.kStowPos)));
-    Driver1.back().onTrue(new InstantCommand(()-> groundPivot.changeSetpoint(GroundPivotConstants.kScorePos)));
-    Driver1.rightStick().whileTrue(new RunCommand(()->groundIntake.setGroundIntake(GroundIntakeConstants.kGroundEjectSpeed)));
   
   Driver1.x().onTrue(new SequentialCommandGroup(
     new ParallelCommandGroup(
