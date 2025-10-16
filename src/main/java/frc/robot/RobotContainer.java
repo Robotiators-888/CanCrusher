f7c580ae5bea5849ff30e734ae0d0fb1945b0d47
ajassi9751 <ajassi9751@gmail.com>
2025-10-16 15:26:45 -0400

    Fixed merge?

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -93,7 +93,14 @@ public class RobotContainer {
       new ParallelCommandGroup(
       new InstantCommand(()-> pneumatics.pistonReverse(), pneumatics))));
   
-    Driver1.y().onTrue(new InstantCommand(()-> pneumatics.pistonToggle(), pneumatics));
+  Driver1.y().onTrue(new InstantCommand(()-> pneumatics.pistonToggle(), pneumatics));
+
+// TODO: add bahner sensor(detect if there is a blockage), E-stop(may not be nessary), speaker(if there is time and avaiable speaker)  
+    // Possibly remove:  Groundintake + Groundpivot (CANbot should not need it)
+    // Functions:  LED(default state and cancrushing sequence. possibly change colors if there is a blockage), 
+    //       Sensor(after time passes(2-5 sec) and object is still there(or in a speficied distance), activate blockage mode)
+    //       Blockage mode(robot disables itself(turn of or disable penuamtics and possibly driving) 
+                  //and lEDs and sound to alert that there is a blockage in the robot) 
   }
 
 
--- src/main/java/frc/robot/subsystems/CANDriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/CANDriveSubsystem.java
@@ -4,30 +4,14 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.SparkMaxConfig;
-
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-// import edu.wpi.first.wpilibj.motorcontrol.Talon; 
+import edu.wpi.first.wpilibj.motorcontrol.Talon; 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX; 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DriveConstants;
 
 // Class to drive the robot over CAN
 public class CANDriveSubsystem extends SubsystemBase {
-  private static CANDriveSubsystem INSTANCE = null;
-  private final SparkMax leftLeader;
-  private final SparkMax leftFollower;
-  private final SparkMax rightLeader;
-  private final SparkMax rightFollower;
-
-  // private final Talon leftleaderTalon;
-  // private final Talon leftfollowerTalon;
-  // private final Talon rightleaderTalon;
-  // private final Talon rightfollowerTalon;
 
   private final DifferentialDrive drive;
 
@@ -43,59 +27,19 @@ public class CANDriveSubsystem extends SubsystemBase {
 
   private CANDriveSubsystem() {
     // create brushed motors for drive
-    leftLeader = new SparkMax(DriveConstants.LEFT_LEADER_ID, MotorType.kBrushed);
-    leftFollower = new SparkMax(DriveConstants.LEFT_FOLLOWER_ID, MotorType.kBrushed);
-    rightLeader = new SparkMax(DriveConstants.RIGHT_LEADER_ID, MotorType.kBrushed);
-    rightFollower = new SparkMax(DriveConstants.RIGHT_FOLLOWER_ID, MotorType.kBrushed);
+    WPI_TalonSRX LeftLeader = new WPI_TalonSRX(DriveConstants.LEFT_LEADER_ID);
+    WPI_TalonSRX RightLeader = new WPI_TalonSRX(DriveConstants.RIGHT_LEADER_ID);
 
-    // WPI_TalonSRX LeftLeader = new WPI_TalonSRX(RobotMap.LEFT_LEADER_ID);
-    // WPI_TalonSRX RightLeader = new WPI_TalonSRX(RobotMap.RIGHT_LEADER_ID);
-
-    // WPI_TalonSRX leftFollow = new WPI_TalonSRX(RobotMap.LEFT_FOLLOWER_ID);
-    // WPI_TalonSRX rightFollow = new WPI_TalonSRX(RobotMap.RIGHT_FOLLOWER_ID);
+    WPI_TalonSRX LeftFollower = new WPI_TalonSRX(DriveConstants.LEFT_FOLLOWER_ID);
+    WPI_TalonSRX RightFollower = new WPI_TalonSRX(DriveConstants.RIGHT_FOLLOWER_ID);
      
-    // leftFollow.follow(leftLeader);
-    // rightFollow.follow(rightLeader);
-
-// TODO: Find what RobotMap is.
-
     // set up differential drive class
-    drive = new DifferentialDrive(leftLeader, rightLeader);
+    drive = new DifferentialDrive(LeftLeader, RightLeader);
+    LeftFollower.follow(LeftLeader);
+    RightFollower.follow(RightLeader);
+    
+//usb cable, weird type,  connect to rio, then phenix (note)
 
-    // Set can timeout. Because this project only sets parameters once on
-    // construction, the timeout can be long without blocking robot operation. Code
-    // which sets or gets parameters during operation may need a shorter timeout.
-    leftLeader.setCANTimeout(250);
-    rightLeader.setCANTimeout(250);
-    leftFollower.setCANTimeout(250);
-    rightFollower.setCANTimeout(250);
-
-    // Create the configuration to apply to motors. Voltage compensation
-    // helps the robot perform more similarly on different
-    // battery voltages (at the cost of a little bit of top speed on a fully charged
-    // battery). The current limit helps prevent tripping
-    // breakers.
-    SparkMaxConfig config = new SparkMaxConfig();
-    config.voltageCompensation(12);
-    config.smartCurrentLimit(DriveConstants.DRIVE_MOTOR_CURRENT_LIMIT);
-
-    // Set configuration to follow leader and then apply it to corresponding
-    // follower. Resetting in case a new controller is swapped
-    // in and persisting in case of a controller reset due to breaker trip
-    config.follow(leftLeader);
-    leftFollower.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    config.follow(rightLeader);
-    rightFollower.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-
-    // Remove following, then apply config to right leader
-    config.disableFollowerMode();
-    rightLeader.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    leftLeader.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    // Set conifg to inverted and then applied to left Follower and right Follower. Set Left side inverted
-    // so that postive values drive both sides forward
-    config.inverted(true);
-    leftFollower.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    rightFollower.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
 
   @Override
